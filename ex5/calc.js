b0971e643fadc735e2da619b828edd63ad5e41f8
Valentyn <rubinvalentin@gmail.com>
2022-12-03 14:25:13 +0100

    first comit

---
+++ ex5/calc.js
@@ -0,0 +1,16 @@
+function sum(a,b){
+    return a+b
+}
+
+function sub(a,b){
+    return a-b
+}
+
+exports.funk1 = sum
+exports.funk2 = sub
+
+// let exports = {
+//     funk1: sum,
+//     funk2: sub
+// }
+//exports.funk1(5,10)
---
+++ ex5/index.js
@@ -0,0 +1,4 @@
+const lib = require('./calc')
+
+console.log(lib.funk1(5,10))
+console.log(lib.funk2(15,3))
